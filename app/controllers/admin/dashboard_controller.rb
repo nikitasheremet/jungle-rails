eaa15451abc408df046f7f537f9dbdfbcbe12741
nikitasheremet <nikita.sheremet253@gmail.com>
2019-10-16 21:54:08 +0000

    add dashboard vount statistics to Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"].to_s, password: ENV["PASS"].to_s
   def show
+    @num_products = Product.count()
+    @num_categories = Category.count()
   end
 end
